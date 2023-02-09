1bc33cd97509e1967681d8c74863f33a85089bf2
mmatsuo <36731954+karakazeviewview@users.noreply.github.com>
2023-02-09 23:14:56 +0900

    Update philosophers.h

--- philosophers.h
+++ philosophers.h
@@ -6,12 +6,20 @@
 /*   By: mmatsuo <mmatsuo@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/09 00:12:51 by mmatsuo           #+#    #+#             */
-/*   Updated: 2023/02/09 14:54:12 by mmatsuo          ###   ########.fr       */
+/*   Updated: 2023/02/09 19:09:21 by mmatsuo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILOSOPHERS_H
 #define PHILOSOPHERS_H
+
+# define MAX_PHILO 200
+
+# define STATE_FORK "has taken a fork"
+# define STATE_EAT "is eating"
+# define STATE_SLEEP "is sleeping"
+# define STATE_THINK "is thinking"
+# define STATE_DIED "died"
 
 # include <limits.h>
 # include <pthread.h>
@@ -20,7 +28,7 @@
 # include <sys/time.h>
 # include <unistd.h>
 
-# define MAX_PHILO 200
+# define ERROR_ARG_INVALID "Error, invalid arg"
 
 typedef struct s_philo
 {
@@ -30,7 +38,8 @@ typedef struct s_philo
 	pthread_mutex_t	*fork_left;
 	pthread_mutex_t	*fork_right;
 	struct s_data	*data;
-	ptrhread_t		thread;
+	pthread_t		thread;
+	long			time_start;
 }	t_philo;
 
 typedef struct s_data
@@ -41,6 +50,8 @@ typedef struct s_data
 	long			time_to_sleep;
 	long			philo_must_eat;
 
+	int				dead_num;
+
 	pthread_mutex_t	philo_mtx[MAX_PHILO];
 	pthread_mutex_t	fork_mtx[MAX_PHILO];
 	pthread_mutex_t	write_finish_mtx;
@@ -48,6 +59,7 @@ typedef struct s_data
 	bool			is_finish;
 	struct s_philo	philo[MAX_PHILO];
 	time_t			time_start;
+	pthread_t		thread_monitor;
 }	t_data;
 
 
